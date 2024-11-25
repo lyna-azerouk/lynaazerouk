fdf2d2a7aa1f972cddc07c98a5c77a885b724cab
Lyna <azerouklyna2001@hotmail.com>
2024-11-25 17:33:47 +0100

    config tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit',
+  content: [
+    "./src/*.{html,js}",
+  ],
   theme: {
     extend: {},
+    fontFamily: {
+      'dmsans': ["roboto", "sans-serif"],
+      'nunito': ['nunito', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif']
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
   },
   plugins: [],
+
 }
 
